2ac2183cd98b21efc8ca3ac81f8722af952ca22d
Kserise <tvsh123@naver.com>
2021-12-28 00:40:54 +0900

    처음 엑티브 제거

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $("#fullpage").fullpage();
 
 const donut = $(".clippath > div");
-donut.removeClass("active").eq(0).addClass("active");
 $(".donuts img").click(function(){
     var index = $(this).index();
     donut.eq(index).insertAfter(".clippath > div:nth-child(3)");
