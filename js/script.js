38b7a1d315ef6c4f98d704adb3f8e6c13a78c9a2
Kserise <tvsh123@naver.com>
2022-01-02 22:25:32 +0900

    220102 마우스무브이벤트, 컨텍트어스, gnb추가

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+let body = true;
+$(".toggle").click(function(){
+    $(".toggle").toggleClass("active");
+    $("nav").toggleClass("active");
+    $(".section").toggleClass("transform");
+    if(body == true){
+        body = false;
+        $("body").on('scroll touchmove mousewheel', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    }else {
+        body = true;
+        $("body").off('scroll touchmove mousewheel');
+    }
+});
+
 let doindex = 0;
 let width;
-
 $("#fullpage").fullpage({
     'onLeave': function (anchorLink, index) {
         if (index == 2){
@@ -28,10 +44,14 @@ $("#fullpage").fullpage({
                     $(".docon").removeClass("active").eq(doindex).addClass("active");
                 });
             }
+            $(".sec2bg").addClass("active");
         }else {
             doindex = 0;
             $(".docon").removeClass("active");
+            $(".sec2bg").removeClass("active");
         }
+
+
         if (index == 3) {
             $(".castle-con").addClass("active");
         }else {
@@ -40,7 +60,25 @@ $("#fullpage").fullpage({
     }
 });
 
+let swidth;
+let height;
+const do1move1 = $("#do1_mmove1");
+const do1move2 = $("#do1_mmove2");
+const do2move1 = $("#do2_mmove1");
+const do2move2 = $("#do2_mmove2");
+const do3move1 = $("#do3_mmove1");
+const do3move2 = $("#do3_mmove2");
 
+$(".sec1").mousemove(function(e){
+    swidth = e.clientX;
+    height = e.clientY;
+    do1move1.css({left:25-(swidth*0.002)+"%", top:50-(height*0.01)+"%"});
+    do1move2.css({left:-300+(swidth*0.01)+"px", top:height*0.1+"px"});
+    do2move1.css({left:25-(swidth*0.002)+"%", top:50-(height*0.01)+"%"});
+    do2move2.css({right:-300+(swidth*0.01)+"px", top:height*0.1+"px"});
+    do3move1.css({left:40-(swidth*0.002)+"%", top:50-(height*0.01)+"%"});
+    do3move2.css({right:-300+(swidth*0.01)+"px", top:height*0.1+"px"});
+});
 
 
 const donut = $(".clippath > div");
