ab3c6d02da8de5386ab4c56b5055242425d2af9d
Kserise <tvsh123@naver.com>
2022-01-08 23:26:38 +0900

    서브페이지 사이드 추가

--- js/sub01.js
+++ js/sub01.js
@@ -2,3 +2,9 @@ $("#discount").click(function(e){
     e.preventDefault();
     $(".items").toggleClass("discount");
 });
+
+$(".submenu > li").click(function(e){
+    e.preventDefault();
+    $(this).find(".sub2menu").toggleClass("active");
+    $(this).toggleClass("active");
+});
